714c2384a6a7a458525a6c4c247770d8a83dc570
Skyelah <skyelahbay@gmail.com>
2018-12-20 22:21:30 -0600

    added scss files for colors, also added my reset styles I use for every project pretty much, and refactored some of the CSS that was given by the generated vue app because it was no longer needed.

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,52 @@
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
 
+
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './colors.scss';
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+  
+  html { font-size: 62.5% }
+  
+  body {
+    box-sizing: border-box;
+    font-size: 1.6rem;
+    width: 100%;
+    min-height: 100vh;
+    position: relative;
+    background-color: map-get($colors, primary-bg);
+  }
+  
+  a		{ text-decoration: none }
+  ul	{ list-style: none }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: map-get($colors, primary-text);
+    margin-top: 60px;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,18 +42,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+@import '../colors.scss';
+
+h3 { margin-top: 40px }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
-}
+a:link, a:visited { color: map-get($colors, a-link) }
+a:hover { color: map-get($colors, a-hover) }
 </style>
